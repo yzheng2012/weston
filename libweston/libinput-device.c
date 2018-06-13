e8b8fe051cb0f491074dff187d3cfbd5d969edcf
Zheng Yang <zhengyang@rock-chips.com>
2018-06-13 10:12:06 +0800

    libinput-device: fix faulty memory access in evdev_device_set_output
    In evdev_device_set_output, output_destroy_listener is removed first if
    the notify function exist. But the prev and next of output_destroy_listener
    is freed when destroying output, then the wl_list_remove action is illegal
    memory operation that writing past the end of a heap block and corrupting
    heap metadata. This issue is reported by valgrind.
    
    ==1266== Invalid write of size 8
    ==1266==    at 0x48A363C: wl_list_remove (wayland-util.c:55)
    ==1266==    by 0x51FE293: evdev_device_set_output (libinput-device.c:538)
    ==1266==    by 0x51FCE1F: notify_output_create (libinput-seat.c:374)
    ==1266==    by 0x486C227: wl_signal_emit (wayland-server-core.h:468)
    ==1266==    by 0x486C227: weston_compositor_add_output (compositor.c:4595)
    ==1266==    by 0x486FADB: weston_output_enable (compositor.c:4938)
    ==1266==    by 0x406537: drm_backend_output_configure (main.c:1096)
    ==1266==    by 0x486F96B: wl_signal_emit (wayland-server-core.h:468)
    ==1266==    by 0x486F96B: weston_compositor_add_pending_output (compositor.c:4851)
    ==1266==    by 0x51FAA6F: create_output_for_connector (compositor-drm.c:5005)
    ==1266==    by 0x51FBD3B: update_outputs (compositor-drm.c:5107)
    ==1266==    by 0x51FBEDB: udev_drm_event (compositor-drm.c:5180)
    ==1266==    by 0x48A06BB: wl_event_source_fd_dispatch (event-loop.c:95)
    ==1266==    by 0x48A0E63: wl_event_loop_dispatch (event-loop.c:641)
    ==1266==  Address 0x50092a8 is 536 bytes inside a block of size 1,208 free'd
    ==1266==    at 0x4844F64: free (vg_replace_malloc.c:530)
    ==1266==    by 0x51FA853: drm_output_destroy (compositor-drm.c:4847)
    ==1266==    by 0x51FBDCB: update_outputs (compositor-drm.c:5127)
    ==1266==    by 0x51FBEDB: udev_drm_event (compositor-drm.c:5180)
    ==1266==    by 0x48A06BB: wl_event_source_fd_dispatch (event-loop.c:95)
    ==1266==    by 0x48A0E63: wl_event_loop_dispatch (event-loop.c:641)
    ==1266==    by 0x489F57B: wl_display_run (wayland-server.c:1260)
    ==1266==    by 0x406D1B: main (main.c:1868)
    ==1266==  Block was alloc'd at
    ==1266==    at 0x4845D88: calloc (vg_replace_malloc.c:711)
    ==1266==    by 0x51FA8A3: zalloc (zalloc.h:38)
    ==1266==    by 0x51FA8A3: create_output_for_connector (compositor-drm.c:4944)
    ==1266==    by 0x51FBF8B: create_outputs (compositor-drm.c:5046)
    ==1266==    by 0x51FCB17: drm_backend_create (compositor-drm.c:5733)
    ==1266==    by 0x51FCDA3: weston_backend_init (compositor-drm.c:5839)
    ==1266==    by 0x4870BC7: weston_compositor_load_backend (compositor.c:5753)
    ==1266==    by 0x404E93: load_drm_backend (main.c:1131)
    ==1266==    by 0x405EDB: load_backend (main.c:1617)
    ==1266==    by 0x406A17: main (main.c:1784)
    
    Signed-off-by: Zheng Yang <zhengyang@rock-chips.com>

--- libweston/libinput-device.c
+++ libweston/libinput-device.c
@@ -535,7 +535,10 @@ evdev_device_set_output(struct evdev_device *device,
 			struct weston_output *output)
 {
 	if (device->output_destroy_listener.notify) {
-		wl_list_remove(&device->output_destroy_listener.link);
+		if (device->output)
+			wl_list_remove(&device->output_destroy_listener.link);
+		else
+			wl_list_init(&device->output_destroy_listener.link);
 		device->output_destroy_listener.notify = NULL;
 	}
 
